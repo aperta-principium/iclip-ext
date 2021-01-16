04e6b93395d682d52b3b9a3d8b68e8fdfbdf928b
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-17 00:31:58 +0100

    Add copy

--- content.js
+++ content.js
@@ -3,9 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
         chrome.tabs.query({active: true, currentWindow: true}, tabs => {
             const url = tabs[0].url;
             const code = document.querySelector("#code");
-            // use `url` here inside the callback because it's asynchronous!
+            const copy = document.querySelector("#copy");
 
-            fetch(`https://interclip.app/includes/api?url=${url}`).then(res => res.json()).then(r => code.innerHTML = r.result);
-            
+            // use `url` here inside the callback because it's asynchronous!
+            fetch(`https://interclip.app/includes/api?url=${url}`).then(res => res.json()).then(r => code.innerHTML = r.result).catch(e => alert(e));
+
+            copy.onclick = () => {
+                navigator.clipboard.writeText(code.innerText)
+                .then(() => {
+                    // Success!
+                })
+                .catch(err => {
+                    alert(err);
+                });
+            };
         });
 });
